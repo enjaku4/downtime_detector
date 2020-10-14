9e5e12e8fbc1d73614a31a006374fa63e0f7cb8f
enjaku4 <enjaku4@gmail.com>
2020-10-15 00:49:08 +0300

    text corrected

--- spec/jobs/ping_job_spec.rb
+++ spec/jobs/ping_job_spec.rb
@@ -7,7 +7,7 @@ describe WebAddresses::PingJob, type: :job do
 
   before { allow(WebAddress).to receive(:find).with(web_address.id).and_return(web_address) }
 
-  context 'pinging is successful' do
+  context 'if pinging is successful' do
     let(:faraday_response_double) { double(status: 200) }
 
     before { allow(Faraday).to receive(:get).with(web_address.url).and_return(faraday_response_double) }
@@ -18,7 +18,7 @@ describe WebAddresses::PingJob, type: :job do
     end
   end
 
-  context 'pinging is unsuccessful' do
+  context 'if pinging is unsuccessful' do
     before { allow(Faraday).to receive(:get).with(web_address.url).and_raise(Faraday::TimeoutError) }
 
     it 'marks the web address as faulty' do
