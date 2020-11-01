b83da88651ac6576e8b95c277baf4c0dab65405a
enjaku <enjaku4@gmail.com>
2020-11-01 14:06:52 +0200

    set up the email sending in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,17 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    user_name: Rails.application.credentials.ses[:smtp][:user_name],
+    password: Rails.application.credentials.ses[:smtp][:password],
+    address: 'email-smtp.eu-west-3.amazonaws.com',
+    port: '25',
+    authentication: :login,
+    enable_starttls_auto: true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
