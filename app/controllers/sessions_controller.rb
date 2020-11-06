bd5c3ab0dfebae5dc6fcac8d2c2526ca234ec320
enjaku4 <enjaku4@gmail.com>
2020-11-06 19:39:40 +0200

    fixed recaptcha related bugs, added recaptcha to web address creation form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   include RecaptchaValidatable
 
   before_action :authenticate_user, only: :destroy
-  before_action :validate_recaptcha, only: :create
+  before_action -> { validate_recaptcha(action: 'sign_in/sign_up') }, only: :create
 
   def create
     outcome = AuthenticationInteraction.run(nickname: params[:nickname], password: params[:password])
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def recaptcha_for(action:)
-    recaptcha_v3(action: 'sign_in/sign_up') if Rails.env.production?
+    recaptcha_v3(action: action) if Rails.env.production?
   end
 end
