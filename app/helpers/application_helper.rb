ba8fc1d50193cb1217f4903753524d3e5abe0952
enjaku <enjaku4@gmail.com>
2020-12-28 13:26:19 +0200

    proper recaptcha specs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def recaptcha_for(action:)
-    recaptcha_v3(action: action, turbolinks: true) if Rails.env.production?
+    recaptcha_v3(action: action, turbolinks: true) if Rails.env.in?(['production', 'test'])
   end
 end
--- spec/controllers/concerns/recaptcha_validatable_spec.rb
+++ spec/controllers/concerns/recaptcha_validatable_spec.rb
@@ -18,31 +18,21 @@ describe RecaptchaValidatable, type: :controller do
   describe '#validate_recaptcha' do
     subject { get :fake_action }
 
-    context "if the environment is \'production\'" do
-      before { allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new('production')) }
+    context 'if the recaptcha is verified' do
+      before { allow_any_instance_of(ApplicationController).to receive(:verify_recaptcha)
+        .with(hash_including(action: 'foo')).and_return(true) }
 
-      context 'if the recaptcha is verified' do
-        before { allow_any_instance_of(ApplicationController).to receive(:verify_recaptcha)
-          .with(hash_including(action: 'foo')).and_return(true) }
-
-        it 'responds with the code 200' do
-          expect(subject).to have_http_status(200)
-        end
-      end
-
-      context 'if the recaptcha is not verified' do
-        before { allow_any_instance_of(ApplicationController).to receive(:verify_recaptcha)
-          .with(hash_including(action: 'foo')).and_return(false) }
-
-        it 'redirects to the root path' do
-          expect(subject).to redirect_to(root_path)
-        end
+      it 'responds with the code 200' do
+        expect(subject).to have_http_status(200)
       end
     end
 
-    context "if the environment is not \'production\'" do
-      it 'responds with the code 200' do
-        expect(subject).to have_http_status(200)
+    context 'if the recaptcha is not verified' do
+      before { allow_any_instance_of(ApplicationController).to receive(:verify_recaptcha)
+        .with(hash_including(action: 'foo')).and_return(false) }
+
+      it 'redirects to the root path' do
+        expect(subject).to redirect_to(root_path)
       end
     end
   end
