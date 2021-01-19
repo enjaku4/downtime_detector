c60a8a7e370b46762c4142787a21ea9a7503e99a
enjaku4 <enjaku4@gmail.com>
2021-01-19 12:14:55 +0200

    increased response timeout

--- app/jobs/web_addresses/ping_job.rb
+++ app/jobs/web_addresses/ping_job.rb
@@ -7,7 +7,7 @@ module WebAddresses
     def perform(web_address_id)
       @web_address = WebAddress.find(web_address_id)
 
-      response = Faraday.get(@web_address.url) { |request| request.options.timeout = 5 }
+      response = Faraday.get(@web_address.url) { |request| request.options.timeout = 10 }
       WebAddresses::Results::ResponseProcessingInteraction.run!(web_address: @web_address, response: response)
     end
   end
