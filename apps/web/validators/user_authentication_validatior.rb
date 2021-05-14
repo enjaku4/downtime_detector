41694c88173f97d43be358c07cebd5b8dd475d12
enjaku4 <enjaku4@gmail.com>
2021-05-14 15:33:23 +0300

    small improvements

--- apps/web/validators/user_authentication_validatior.rb
+++ apps/web/validators/user_authentication_validatior.rb
@@ -4,8 +4,8 @@ module Web
       include Hanami::Validations::Form
 
       validations do
-        required(:nickname) { filled? & str? & min_size?(6) }
-        required(:password) { filled? & str? & min_size?(6) }
+        required(:nickname) { str? & filled? & min_size?(6) }
+        required(:password) { str? & filled? & min_size?(6) }
       end
     end
   end
