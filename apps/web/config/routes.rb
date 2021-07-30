c71f899091e4c2bc4c201e2b84949d76b1aa8be3
enjaku4 <32835381+enjaku4@users.noreply.github.com>
2021-07-30 17:01:09 +0300

    sidekiq (#5)

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -3,7 +3,10 @@
 #
 # Example:
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
+require 'sidekiq/web'
 
 root to: 'web_addresses#index'
 
 resources :web_addresses, except: [:index, :edit, :update]
+
+mount Sidekiq::Web, at: '/sidekiq' if Hanami.env?(:development)
