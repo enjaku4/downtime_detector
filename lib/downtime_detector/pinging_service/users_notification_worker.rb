f1a75d4998b8c0a9c87f494357d4df197c22fda1
enjaku4 <enjaku4@gmail.com>
2021-07-31 12:03:31 +0300

    fixed notification worker

--- lib/downtime_detector/pinging_service/users_notification_worker.rb
+++ lib/downtime_detector/pinging_service/users_notification_worker.rb
@@ -3,7 +3,7 @@ module PingingService
     include Sidekiq::Worker
 
     def perform(web_address_id)
-      web_address = WebAddressRepository.new.find_with_users(id)
+      web_address = WebAddressRepository.new.find_with_users(web_address_id)
 
       web_address.users.each do |user|
         next if Hanami::Utils::Blank.blank?(user.email)
