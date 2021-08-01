b9bc9d22751e8502165bb8e669d0a3f76f2cb188
enjaku4 <enjaku4@gmail.com>
2021-08-01 18:24:39 +0300

    probably this will work

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -6,9 +6,11 @@ Airbrake.configure do |c|
   c.project_id = ENV['AIRBRAKE_PROJECT_ID']
   c.project_key = ENV['AIRBRAKE_API_KEY']
   c.environment = Hanami.env
-  c.ignore_environments = [:development, :test]
+  c.ignore_environments = [:test]
 end
 
 Airbrake.add_filter do |notice|
-  notice.ignore! if notice.stash[:exception].is_a?(SignalException)
+  if notice[:errors].any? { |error| error[:type] == 'SignalException' }
+    notice.ignore!
+  end
 end
