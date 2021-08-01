7273e71b3321d9dfa1d955710ce9612fd31be01f
enjaku4 <enjaku4@gmail.com>
2021-08-01 18:25:05 +0300

    oops

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -6,7 +6,7 @@ Airbrake.configure do |c|
   c.project_id = ENV['AIRBRAKE_PROJECT_ID']
   c.project_key = ENV['AIRBRAKE_API_KEY']
   c.environment = Hanami.env
-  c.ignore_environments = [:test]
+  c.ignore_environments = [:development, :test]
 end
 
 Airbrake.add_filter do |notice|
